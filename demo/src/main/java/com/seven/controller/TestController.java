30acb5cb897a34413200326f1eb3bf10ae81efe2
dzydzy7 <dzydzy_7@163.com>
2020-10-17 15:42:31 +0800

    change

--- demo/src/main/java/com/seven/controller/TestController.java
+++ demo/src/main/java/com/seven/controller/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping(value = "/test")
     public String test(){
+// 注释
         return "test controller";
     }
 }
